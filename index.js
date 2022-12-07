81d459c9b1cc669788a5d7b2f41d91964b56b034
tugi-ngunjiri <blueqwinn@gmail.com>
2022-12-07 07:53:44 +0300

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+function getPups()
+fetch;Pups;
+fetch;"https://curriculum-content.s3.amazonaws.com/js/woof-woof/dog_1.jpg"
+console.log(Pups)
+
+document.getElementById("myBtn").
+addEventListener("click",
+displayDate);
